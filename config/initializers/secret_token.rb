ee63f80d192f525c0f53339bd62a6786d891af61
ynos1234 <ynos1234@gmail.com>
2014-08-08 14:52:15 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaCorsa::Application.config.secret_token = '80434a236baa6f38249dbfdb0bc4352779b76d7cc7499fe790733ed4f70c822488e45011048d6867450be7ee01a8f12123385fc1c2818fbb501c7a454e4ba5a5'
